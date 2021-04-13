579a7e0c400ae9513972707ac28f545a8ff32841
AliD201 <AliD201@users.noreply.github.com>
2021-04-13 09:09:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,55 @@
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8002";
+
+// this is just an example of 2fa auth
+var twoWayAuth = {
+        auth12345:{
+            id : "12345",
+            verificationNumber: '54321',
+        }
+    }
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Routes Definitions
+ */
+// app.get("/", (req, res) => {
+//     res.status(200).send("WHATABYTE: Food For Devs");
+//   });
+
+app.get("/", (req, res) => {
+    res.render('pages/index.ejs');
+});
+app.get("/login", (req, res) => {
+    res.render('pages/index.ejs');
+});
+app.get("/register", (req, res) => {
+    res.render('pages/index.ejs');
+});
+app.get("/2fa/:id:", (req, res) => {
+    const {id} = req.params()
+    res.render('pages/index.ejs');
+});
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
